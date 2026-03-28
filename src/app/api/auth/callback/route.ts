25436e658cff3c401041f5e854def33af1bea8e5
NicheFlow1 <fitnessshoes9@gmail.com>
2026-03-28 13:12:47 +0100

    add: src/app/api/auth/callback/route.ts

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,3 @@
+import { NextRequest,NextResponse } from 'next/server'
+import { createClient } from '@/lib/supabase/server'
+export async function GET(req:NextRequest){const {searchParams,origin}=new URL(req.url);const code=searchParams.get('code'),next=searchParams.get('next')||'/dashboard';if(code){const supabase=createClient();const {error}=await supabase.auth.exchangeCodeForSession(code);if(!error)return NextResponse.redirect(`${origin}${next}`)};return NextResponse.redirect(`${origin}/auth/login?error=failed`)}
