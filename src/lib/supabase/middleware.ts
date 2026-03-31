8ad75f42225863723376a9eb1d7e904318b1bcf3
NicheFlow1 <fitnessshoes9@gmail.com>
2026-04-01 01:28:32 +0200

    fix: src/lib/supabase/middleware.ts

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -3,8 +3,8 @@ import { NextResponse, type NextRequest } from 'next/server'
 export async function updateSession(request: NextRequest) {
   let response = NextResponse.next({ request })
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL ?? 'https://aincmpxokmsygyghvtnm.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpbmNtcHhva21zeWd5Z2h2dG5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQyODQ4NzAsImV4cCI6MjA4OTg2MDg3MH0.qy9k6S3pgNv7CPnvJlgqeGzgzHBB0J59cCWVsbSa75U',
     {
       cookies: {
         getAll() { return request.cookies.getAll() },
